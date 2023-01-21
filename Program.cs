e38c0486f13eb75a4668a7676b2fe900a9aceec2
Denis Gromenko <dvgromenko@gmail.com>
2023-01-21 11:28:38 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public interface IStrategy
+{
+    public string Algorithm();
+}
+
+
+public class Context
+{
+    
+}
